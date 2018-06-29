096db977cd7ae6b009330071642d20b77d09b271
vygandas <vygandas.pliasas@gmail.com>
2018-06-29 22:38:36 +0300

    get last user id function fix

--- src/helpers/userListHelpers.ts
+++ src/helpers/userListHelpers.ts
@@ -1,6 +1,9 @@
 import {IUser} from "../interfaces/IUser";
 
 export const getLastUserIdFromList = (users: IUser[]) => {
-    const lastUser = users.slice(-1)[0];
-    return typeof lastUser !== typeof undefined && typeof lastUser === typeof IUser ? lastUser.id : 0;
+    if (users != null && users.length > 0) {
+        const lastUser = users[users.length - 1];
+        return typeof lastUser.id !== typeof undefined ? lastUser.id : 0;
+    }
+    return 0;
 };
