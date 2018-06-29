611b3eb333d858bdb7ed12d8eac5c3454fc87545
vygandas <vygandas.pliasas@gmail.com>
2018-06-29 20:49:25 +0300

    hoc preloader component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import "assets/scss/App.scss";
 import Header from "./Header/index";
+import Preloader from "./Preloader/index";
 
 export interface AppProps {
 }
@@ -9,7 +10,9 @@ export default class App extends React.Component<AppProps, undefined> {
     render() {
         return (
             <div className="app-component">
-                <Header/>
+                <Preloader>
+                    <Header/>
+                </Preloader>
             </div>
         );
     }
